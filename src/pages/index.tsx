660d3871d67d6ee752a4a54db891c63ca7724da7
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-04-25 20:46:35 +0900

    refactor: Replaced useState/useEffect with useSWR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,33 @@
-import React, {useEffect, useState} from "react";
-export default function Home() {
-  const [nowplaying, setNowplaying] = useState(false)
-  const [artist, setArtist] = useState('')
-  const [name, setName] = useState('')
-  const [album, setAlbum] = useState('')
-  const [url, setUrl] = useState('')
+import React from 'react';
+import useSWR from "swr";
 
-  useEffect(() => {
-    async function getNowPlaying() {
-      const response = await fetch('/api/lastfm')
-      const data = await response.json()
-      if (response.status !== 200) {
-        setNowplaying(false)
-        return
-      }
-      setNowplaying(true)
-      setArtist(data.artist)
-      setName(data.name)
-      setAlbum(data.album)
-      setUrl(data.url)
-    }
-    getNowPlaying()
-  }, [])
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+export default function Home() {
+  const {data,error} = useSWR("/api/lastfm", fetcher);
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
+  const {artist,name,album,url} = data;
+  const nowplaying = Boolean(artist || name || album || url);
 
   return (
     <div>
-      <h1><a href={'https://www.last.fm/ja/user/shiyui'}>Shiyui</a> Now Playing</h1>
+      <h1>
+        <a href="https://www.last.fm/ja/user/shiyui">Shiyui</a> Now Playing
+      </h1>
       <hr />
-      <p>{nowplaying ? '': 'Not'} Listening to Music</p>
-      <div style={{display: nowplaying ? 'block' : 'none'}}>
-      <p>Artist: {artist}</p>
-      <p>Name: {name}</p>
-      <p>Album: {album}</p>
-      <p>Music URL: <a href={url}>last.fm</a></p>
-      <hr />
+      <p>{nowplaying ? "" : "Not"} Listening to Music</p>
+      <div style={{ display: nowplaying ? "block" : "none" }}>
+        <p>Name: {name}</p>
+        <p>Artist: {artist}</p>
+        <p>Album: {album}</p>
+        <p>
+          Music URL: <a href={url}>last.fm</a>
+        </p>
+        <hr />
       </div>
     </div>
-  )
+  );
 }
-
